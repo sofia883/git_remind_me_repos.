2882d19e2ae7476991575a62c7c3d7d0363f2e49
sofia883 <girlskhan053@gmail.com>
2024-08-04 03:35:26 -0700

    resolved expired reminder problem

--- lib/added_reminders_page.dart
+++ lib/added_reminders_page.dart
@@ -28,7 +28,6 @@ class _AddedRemindersPageState extends State<AddedRemindersPage> {
   }
 
   void _scheduleNextExpirationCheck() {
-    // Calculate the time remaining until the next full minute
     DateTime now = DateTime.now();
     DateTime nextMinute =
         DateTime(now.year, now.month, now.day, now.hour, now.minute)
@@ -54,9 +53,6 @@ class _AddedRemindersPageState extends State<AddedRemindersPage> {
         .map((reminder) => Map<String, String>.from(jsonDecode(reminder)))
         .toList();
 
-    // Process to remove expired reminders
-    _processExpiredReminders();
-
     setState(() {
       reminders = loadedReminders;
       expiredReminders = loadedExpiredReminders;
@@ -64,10 +60,16 @@ class _AddedRemindersPageState extends State<AddedRemindersPage> {
 
     print('Reminders loaded: $reminders');
     print('Expired reminders loaded: $expiredReminders');
+
+    // Immediately process expired reminders upon loading
+    _processExpiredReminders();
   }
 
-  void _processExpiredReminders() {
-    DateTime now = DateTime.now();
+  void _processExpiredReminders() async {
+    DateTime now = DateTime.now().subtract(Duration(
+        seconds: DateTime.now().second,
+        microseconds:
+            DateTime.now().microsecond)); // Round down to the nearest minute
     List<Map<String, String>> newExpiredReminders = [];
 
     reminders.removeWhere((reminder) {
@@ -81,7 +83,6 @@ class _AddedRemindersPageState extends State<AddedRemindersPage> {
       return false;
     });
 
-    // Update the expired reminders list and save changes
     setState(() {
       expiredReminders.addAll(newExpiredReminders);
     });
@@ -103,20 +104,84 @@ class _AddedRemindersPageState extends State<AddedRemindersPage> {
     await prefs.setStringList('expiredReminders', expiredRemindersData);
   }
 
-  void _removeExpiredReminders() {
+  DateTime? _parseDateTime(String date, String time) {
+    try {
+      DateTime parsedDate = DateFormat('d MMMM yyyy').parse(date);
+
+      List<String> timeParts = time.split(':');
+      int hour = int.parse(timeParts[0]);
+      int minute = int.parse(timeParts[1].split(' ')[0]);
+
+      if (time.toLowerCase().contains('pm') && hour != 12) {
+        hour += 12;
+      } else if (time.toLowerCase().contains('am') && hour == 12) {
+        hour = 0;
+      }
+
+      return DateTime(
+          parsedDate.year, parsedDate.month, parsedDate.day, hour, minute);
+    } catch (e) {
+      print('Error parsing date or time: $e');
+      return null;
+    }
+  }
+
+  Future<void> _selectDate(
+      BuildContext context, TextEditingController controller) async {
     DateTime now = DateTime.now();
-    setState(() {
-      reminders.removeWhere((reminder) {
-        DateTime? reminderDateTime =
-            _parseDateTime(reminder['date']!, reminder['time']!);
-        if (reminderDateTime != null && reminderDateTime.isBefore(now)) {
-          expiredReminders.add(reminder);
-          return true;
-        }
-        return false;
-      });
-      _saveReminders();
-    });
+    DateTime initialDate = DateTime(now.year, now.month, now.day);
+
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: initialDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDate != null) {
+      controller.text = DateFormat('d MMMM yyyy').format(pickedDate);
+    }
+  }
+
+  Future<void> _selectTime(
+      BuildContext context, TextEditingController controller) async {
+    TimeOfDay now = TimeOfDay.now();
+    TimeOfDay initialTime = TimeOfDay(hour: now.hour, minute: now.minute);
+
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: initialTime,
+    );
+
+    if (pickedTime != null) {
+      controller.text = pickedTime.format(context);
+    }
+  }
+
+  void _confirmDeleteReminder(int index) async {
+    final result = await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Delete Reminder'),
+          content: Text('Are you sure you want to delete this reminder?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (result == true) {
+      _deleteReminder(index);
+    }
   }
 
   void _deleteReminder(int index) async {
@@ -187,11 +252,8 @@ class _AddedRemindersPageState extends State<AddedRemindersPage> {
             ),
             TextButton(
               onPressed: () {
-                // Validate date and time before saving
-                DateTime? selectedDateTime = _parseDateTime(
-                  dateController.text,
-                  timeController.text,
-                );
+                DateTime? selectedDateTime =
+                    _parseDateTime(dateController.text, timeController.text);
 
                 if (selectedDateTime == null ||
                     selectedDateTime.isBefore(DateTime.now())) {
@@ -231,7 +293,6 @@ class _AddedRemindersPageState extends State<AddedRemindersPage> {
                   _saveReminders();
                 });
 
-                // Show success SnackBar after saving
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
                     content: Text('Reminder edited successfully!'),
@@ -246,96 +307,6 @@ class _AddedRemindersPageState extends State<AddedRemindersPage> {
     );
   }
 
-  DateTime? _parseDateTime(String date, String time) {
-    try {
-      // Parse the date
-      DateTime parsedDate = DateFormat('d MMMM yyyy').parse(date);
-
-      // Parse the time
-      List<String> timeParts = time.split(':');
-      int hour = int.parse(timeParts[0]);
-      int minute = int.parse(timeParts[1].split(' ')[0]);
-
-      // Adjust hour for AM/PM
-      if (time.toLowerCase().contains('pm') && hour != 12) {
-        hour += 12;
-      } else if (time.toLowerCase().contains('am') && hour == 12) {
-        hour = 0;
-      }
-
-      // Combine date and time
-      return DateTime(
-        parsedDate.year,
-        parsedDate.month,
-        parsedDate.day,
-        hour,
-        minute,
-      );
-    } catch (e) {
-      print('Error parsing date or time: $e');
-      return null;
-    }
-  }
-
-  Future<void> _selectDate(
-      BuildContext context, TextEditingController controller) async {
-    DateTime now = DateTime.now();
-    DateTime initialDate = DateTime(now.year, now.month, now.day);
-
-    DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: initialDate,
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
-
-    if (pickedDate != null) {
-      controller.text = DateFormat('d MMMM yyyy')
-          .format(pickedDate); // Adjust format as needed
-    }
-  }
-
-  Future<void> _selectTime(
-      BuildContext context, TextEditingController controller) async {
-    TimeOfDay now = TimeOfDay.now();
-    TimeOfDay initialTime = TimeOfDay(hour: now.hour, minute: now.minute);
-
-    TimeOfDay? pickedTime = await showTimePicker(
-      context: context,
-      initialTime: initialTime,
-    );
-
-    if (pickedTime != null) {
-      controller.text = pickedTime.format(context);
-    }
-  }
-
-  void _confirmDeleteReminder(int index) async {
-    final result = await showDialog<bool>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Delete Reminder'),
-          content: Text('Are you sure you want to delete this reminder?'),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false),
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(true),
-              child: Text('Delete'),
-            ),
-          ],
-        );
-      },
-    );
-
-    if (result == true) {
-      _deleteReminder(index);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -343,164 +314,35 @@ class _AddedRemindersPageState extends State<AddedRemindersPage> {
         title: Text('Added Reminders'),
         actions: [
           IconButton(
-            icon: Icon(Icons.history),
+            icon: Icon(Icons.delete),
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ExpiredRemindersPage(expiredReminders),
-                ),
-              );
+              setState(() {
+                expiredReminders
+                    .clear(); // Clear expired reminders from the list
+                _saveReminders();
+              });
             },
           ),
         ],
       ),
-      body: reminders.isEmpty
-          ? Center(
-              child: Text(
-                'No reminders added yet.',
-                style: TextStyle(fontSize: 18, color: Colors.grey),
+      body: ListView.builder(
+        itemCount: reminders.length,
+        itemBuilder: (context, index) {
+          final reminder = reminders[index];
+          return Card(
+            child: ListTile(
+              title: Text(reminder['title'] ?? 'No Title'),
+              subtitle: Text(
+                  '${reminder['description']}\n${reminder['date']} ${reminder['time']}'),
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () => _confirmDeleteReminder(index),
               ),
-            )
-          : ListView.builder(
-              padding: const EdgeInsets.all(8.0),
-              itemCount: reminders.length,
-              itemBuilder: (context, index) {
-                final reminder = reminders[index];
-                final dateTimeText =
-                    reminder['date'] != null && reminder['time'] != null
-                        ? '${reminder['date']} at ${reminder['time']}'
-                        : '';
-
-                return Card(
-                  elevation: 20,
-                  shadowColor: Colors.black,
-                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                    side: BorderSide(color: Colors.grey, width: 2.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          reminder['title'] ?? '',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8.0),
-                        Text(
-                          reminder['description'] ?? '',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(height: 8.0),
-                        Text(
-                          'Scheduled time:',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 4.0),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    reminder['date'] ?? '',
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.grey[600],
-                                    ),
-                                  ),
-                                  Text(
-                                    reminder['time'] ?? '',
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.orange,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                IconButton(
-                                  icon: Icon(Icons.edit, color: Colors.blue),
-                                  onPressed: () => _editReminder(index),
-                                ),
-                                Container(
-                                  height:
-                                      24.0, // Adjust the height to fit the icons properly
-                                  child: VerticalDivider(
-                                    color: Colors.grey,
-                                    thickness: 1,
-                                    width: 20,
-                                  ),
-                                ),
-                                IconButton(
-                                  icon:
-                                      Icon(Icons.delete, color: Colors.orange),
-                                  onPressed: () =>
-                                      _confirmDeleteReminder(index),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
+              onTap: () => _editReminder(index),
             ),
-    );
-  }
-}
-
-class ExpiredRemindersPage extends StatelessWidget {
-  final List<Map<String, String>> expiredReminders;
-
-  ExpiredRemindersPage(this.expiredReminders);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Expired Reminders'),
+          );
+        },
       ),
-      body: expiredReminders.isEmpty
-          ? Center(
-              child: Text(
-                'No expired reminders.',
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            )
-          : ListView.builder(
-              padding: const EdgeInsets.all(8.0),
-              itemCount: expiredReminders.length,
-              itemBuilder: (context, index) {
-                final reminder = expiredReminders[index];
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  child: ListTile(
-                    title: Text(reminder['title'] ?? ''),
-                    subtitle: Text(
-                        '${reminder['description'] ?? ''}\n${reminder['date']} at ${reminder['time']}'),
-                    isThreeLine: true,
-                  ),
-                );
-              },
-            ),
     );
   }
 }
